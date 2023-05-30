42f02ae1c79e4bf94ade8a099e8f26ebfa387b44
ngothehung <ngothehung2110@gmail.com>
2023-05-30 12:23:44 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductAddComponent } from './pages/product-add/product-add.component';
+import { ProductEditComponent } from './pages/product-edit/product-edit.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+const routes: Routes = [
+  { path: "", component: HomePageComponent },
+  {
+    path: "admin", component: AdminLayoutComponent, children: [
+      { path: "product/add", component: ProductAddComponent},
+      { path: "product/:id/edit", component: ProductEditComponent}
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductAddComponent } from './pages/product-add/product-add.component';
+import { ProductEditComponent } from './pages/product-edit/product-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminLayoutComponent
+    AdminLayoutComponent,
+    HomePageComponent,
+    ProductAddComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
